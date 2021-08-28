b40cd2c27c2be971f95572768df3b5dd876912e8
izbaris <zaferrosh@gmail.com>
2021-08-28 23:27:26 +0300

    partials created

--- app.js
+++ app.js
@@ -1,13 +1,49 @@
 //import section
 const express = require("express");
+const ejs = require("ejs");
+
+const app = express();
+
+//template engine
+app.set("view engine","ejs");
+
 
 // middlewares
-const app = express();
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.send("INDEX SAYFASI");
+
+app.get("/about", (req, res) => {
+  res.status(200).render("about",{
+      page_name:"about"
+  })
 });
+
+app.get("/login", (req, res) => {
+    res.status(200).render("login",{
+        page_name:"login"
+    })
+  });
+
+  
+app.get("/", (req, res) => {
+    res.status(200).render("index",{
+        page_name:"index"
+    })
+  });
+    
+app.get("/index", (req, res) => {
+    res.status(200).render("index",{
+        page_name:"index"
+    })
+  });
+
+  
+app.get("/contact", (req, res) => {
+    res.status(200).render("contact",{page_name:"contact"})
+  });
+
+  
+
 
 const port = 3000;
 
