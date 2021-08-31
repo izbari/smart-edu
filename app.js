dedc716a02eb3903dc14c18e25e61c51af139648
izbaris <zaferrosh@gmail.com>
2021-08-31 20:33:34 +0300

    course deletion operation

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
+const methodOverride = require('method-override');
 
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
@@ -12,42 +13,51 @@ const userRoute = require('./routes/userRoute');
 const app = express();
 
 //Connect DB
-mongoose.connect('mongodb://localhost/smartedu-db', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
-}).then(()=> {
-  console.log('DB Connected Successfully')
-});
+mongoose
+  .connect('mongodb://localhost/smartedu-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('DB Connected Successfully');
+  });
 
 //Template Engine
 app.set('view engine', 'ejs');
-
 
 global.userIN = null;
 
 //Middlewares
 
 app.use(express.static('public'));
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-app.use(session({
-  secret: 'my_keyboard_cat',
-  resave: false,
-  saveUninitialized: true,
-  store: MongoStore.create({ mongoUrl: 'mongodb://localhost/smartedu-db' })
-}))
-app.use(flash())
-//Routes
-app.use((req,res, next)=>{
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(
+  session({
+    secret: 'my_keyboard_cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: 'mongodb://localhost/smartedu-db' }),
+  })
+);
+app.use(flash());
+app.use((req, res, next) => {
   res.locals.flashMessages = req.flash();
   next();
-})
-app.use("*",(req,res,next)=>{
-  userIN=req.session.userID;
+});
+app.use('*', (req, res, next) => {
+  userIN = req.session.userID;
   next();
-})
+});
+app.use(
+  methodOverride('_method', {
+    methods: ['GET', 'POST'],
+  })
+);
+//Routes
+
 app.use('/', pageRoute);
 app.use('/courses', courseRoute);
 app.use('/categories', categoryRoute);
