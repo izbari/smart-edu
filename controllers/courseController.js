b747f056dff607819a38260c2036150f430b1a88
izbaris <zaferrosh@gmail.com>
2021-08-31 23:54:38 +0300

    course delete | update operations

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -5,7 +5,14 @@ const User = require('../models/User');
 exports.createCourse = async (req, res) => {
   try {
     req.body.user = req.session.userID;
-    const course = await Course.create(req.body);
+    const course = await Course.create({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      user: req.session.userID,
+
+
+    });
     req.flash('success',`${course.name} has been created successfully`);
     res.status(201).redirect('courses');
   } catch (error) {
@@ -101,10 +108,27 @@ exports.releaseCourse = async (req, res) => {
     });
   }
 };
-exports.deleteCourse = async (req, res) => {
+exports.deleteCourse = async (req, res) => { 
   try {
     const course =await Course.findOneAndRemove({slug:req.params.slug})
     req.flash('error',`${course.name} has been removed successfully`);
+    res.status(200).redirect('/users/dashboard');
+
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+exports.updateCourse = async (req, res) => { 
+  try {
+    const course =await Course.findOne({slug:req.params.slug})
+    course.name= req.body.name;
+    course.description= req.body.description;
+    course.category= req.body.category;
+    await course.save();
+
     res.status(200).redirect('/users/dashboard');
 
   } catch (error) {
